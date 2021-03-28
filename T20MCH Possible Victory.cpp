64b0c0ea78cb544278598079b3eb2c8ff7d5b6b9
Mosharrof8413 <50889365+Mosharrof8413@users.noreply.github.com>
2021-03-28 22:20:25 +0600

    Add files via upload

---
+++ T20MCH Possible Victory.cpp
@@ -0,0 +1,50 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define mk make_pair
+#define mxx 10000
+int M;
+ll mod(ll x)
+{
+    return ((x%M+M)%M);
+}
+ll mul(ll a,ll b)
+{
+    return mod(mod(a)*mod(b));
+}
+ll big_mod(ll base,ll power,ll m)
+{
+    ll res=1;
+    if(base==0)return 0;
+    if(power==0)return 1LL;
+    if(power==1)return mod(base);
+    while(power)
+    {
+        if(power%2==1)
+         {
+             res= mul(res,base);
+         }
+         base=mul(base,base);
+         power/=2;
+    }
+    return res;
+}
+int main()
+{
+  ll t=1;
+   //cin>>t;
+  while(t--)
+  {
+      ll r,o,c;
+      cin>>r>>o>>c;
+      ll re=20-o;
+      re*=36;
+      re+=c;
+      if(re>r)cout<<"YES"<<endl;
+      else cout<<"NO"<<endl;
+  }
+}
+
+//Alhamdulillah...
